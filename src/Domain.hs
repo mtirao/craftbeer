6d5e67a0bc356819e40a5d95ba729fdfc4d2c526
Marcos Tirao <>
2020-02-18 18:26:05 -0300

    Update database model

--- src/Domain.hs
+++ src/Domain.hs
@@ -1,4 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE RecordWildCards       #-}
 
 module Domain where
 
@@ -7,6 +13,21 @@ import Data.Aeson
 import Control.Applicative
 
 data Article = Article Integer Text Text -- id title bodyText
+     deriving (Show)
+
+data Detail = Detail Integer Integer Integer Integer -- id recipe_id ingredient_id quantity
+     deriving (Show)
+
+data Ingredient = Ingredient Integer Text Text Text Integer -- id ident name type unit
+     deriving (Show)
+
+data Recipe = Recipe Integer Text Text Text Integer Integer Integer -- id ident stype name ibu abv color
+     deriving (Show)
+
+data Stage = Stage Integer Integer Integer Integer Integer -- id recipe_id type temp time
+     deriving (Show)
+
+data User = User Integer Text Text Text Text -- id ident password role email
      deriving (Show)
 
 instance FromJSON Article where
