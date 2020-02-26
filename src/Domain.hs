85b7d8fa16c702412b3a3c3672663edb68631c9c
Marcos Tirao <>
2020-02-26 14:40:57 -0300

    Add model definition

--- src/Domain.hs
+++ src/Domain.hs
@@ -15,21 +15,7 @@ import Control.Applicative
 data Article = Article Integer Text Text -- id title bodyText
      deriving (Show)
 
-data Detail = Detail Integer Integer Integer Integer -- id recipe_id ingredient_id quantity
-     deriving (Show)
-
-data Ingredient = Ingredient Integer Text Text Text Integer -- id ident name type unit
-     deriving (Show)
-
-data Recipe = Recipe Integer Text Text Text Integer Integer Integer -- id ident stype name ibu abv color
-     deriving (Show)
-
-data Stage = Stage Integer Integer Integer Integer Integer -- id recipe_id type temp time
-     deriving (Show)
-
-data User = User Integer Text Text Text Text -- id ident password role email
-     deriving (Show)
-
+-- Json instance definition
 instance FromJSON Article where
      parseJSON (Object v) = Article <$>
                             v .:? "id" .!= 0 <*> -- the field "id" is optional
@@ -41,4 +27,59 @@ instance ToJSON Article where
          object ["id" .= id,
                  "title" .= title,
                  "bodyText" .= bodyText]
+
+-- Data Detail definition
+data Detail = Detail Integer Integer Integer Integer -- id recipe_id ingredient_id quantity
+     deriving (Show)
+
+-- Json instance definition
+instance FromJSON Detail where
+     parseJSON (Object v) = Detail <$>
+                            v .:? "id" .!= 0 <*> 
+                            v .:  "recipe_id"    <*>
+                            v .:  "ingredient_id" <*>
+                            v .:  "quantity"
+
+instance ToJSON Detail where
+     toJSON (Detail id recipe_id ingredient_id quantity) =
+         object ["id" .= id,
+                 "recipe_id" .= recipe_id,
+                 "ingredient_id" .= ingredient_id ,
+                 "quantity" .=  quantity]
+
+
+-- Data Ingredient definition
+data Ingredient = Ingredient Integer Text Text Text Integer -- id ident name type unit
+     deriving (Show)
+
+-- Json instance definition
+instance FromJSON Ingredient where
+     parseJSON (Object v) = Ingredient <$>
+                            v .:? "id" .!= 0 <*> 
+                            v .:  "ident"    <*>
+                            v .:  "name" <*>
+                            v .:  "type" <*>
+                            v .: "unit"
+
+instance ToJSON Ingredient where
+     toJSON (Ingredient id ident name type_b unit) =
+         object ["id" .= id,
+                 "ident" .= ident,
+                 "name" .= name ,
+                 "type" .=  type_b,
+                 "unit" .= unit]
+
+-- Data Recipe definition
+data Recipe = Recipe Integer Text Text Text Integer Integer Integer -- id ident stype name ibu abv color
+     deriving (Show)
+
+-- Data Stage definition
+data Stage = Stage Integer Integer Integer Integer Integer -- id recipe_id type temp time
+     deriving (Show)
+
+-- Data User definition
+data User = User Integer Text Text Text Text -- id ident password role email
+     deriving (Show)
+
+
                  
